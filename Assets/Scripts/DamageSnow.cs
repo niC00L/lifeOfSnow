33fe7d52b93c27520d721a7c84fa642c72ea11fc
Linzee <d.gmiterko@gmail.com>
2015-10-11 20:21:07 +0200

    Carrot

---
+++ Assets/Scripts/DamageSnow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageSnow : MonoBehaviour {
+
+	public float voidDamageHeight = -50.0f;
+	public float voidDamage = 1.0f;
+	public float touchDamage = 0.4f;
+
+	private Spawn spawn;
+	private SnowballController snowballController;
+
+	void Start () {
+		spawn = GameObject.Find("Spawn").GetComponent<Spawn>();
+		snowballController = this.GetComponent<SnowballController>();
+	}
+
+	void Update () {
+		if (snowballController.transform.position.y < this.voidDamage) {
+			Damage(voidDamage);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		if (collision.gameObject.tag == "Enemy") {
+			Damage(touchDamage);
+		}
+		
+	}
+
+	void Damage(float damage) {
+		snowballController.size -= damage;
+		if (snowballController.size > 0) {
+			snowballController.updateSize ();
+		} else {
+			spawn.spawnSnowball();
+			Destroy(snowballController.gameObject);
+		}
+	}
+}

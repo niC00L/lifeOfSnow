7c83df0286702372200375761d5754e091a01c1e
niC00L <nika.blanarova@gmail.com>
2019-06-21 13:59:33 +0200

    Didn't want to remove this

---
+++ Assets/Scripts/FollowSimple.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowSimple : MonoBehaviour {
+	
+	public Transform target;
+	public float distance = 1.0f;
+	public float height = 1.0f;
+
+	void Start () {
+		
+		if (!target)
+			return;
+
+	}
+	
+	void Update () {
+		
+		if (!target)
+			return;
+
+		float wantedHeight = target.position.y + height;		
+		
+		Vector3 pos = target.position;
+		pos -= transform.TransformDirection(Vector3.forward) * distance;
+		pos.y = wantedHeight;
+		
+		transform.position = pos;
+	}
+}
